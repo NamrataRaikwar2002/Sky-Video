d27f882d232e7ede395cbee135702caa56386929
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-04-11 15:33:23 +0530

    feat: Navbar component

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className="nav_div">
+      <nav className="navigation">
+        <div className="hamburgerNameDiv">
+          <i class="fa-solid fa-bars nav_icon"></i>
+          <h2 className="textForPrimaryColor">
+            Sky<span className="">Video</span>
+          </h2>
+        </div>
+        <div className="search_icon">
+          <input
+            type="text"
+            className="search_bar"
+            placeholder="Search..."
+            name="search"
+          />
+          <i className="fa fa-search"></i>
+        </div>
+        <aside className="nav_rightside">
+          <Link to="/" className="navbar_link textForPrimaryColor productsText">
+            Explore
+          </Link>
+          <button className='btn card_btn'>
+
+            login
+          </button>
+          {/* will use this icon later */}
+            {/* <i class="fa-solid fa-user nav_icon"></i> */}
+        </aside>
+      </nav>
+    </div>
+  )
+}
+
+export { Navbar }
