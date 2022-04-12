b74714a6a8d025e2d242d42fba1125e93766b7e8
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-04-12 14:39:17 +0530

    feat: fetched videos

--- src/Pages/VideoListing/Explore.jsx
+++ src/Pages/VideoListing/Explore.jsx
@@ -1,49 +1,46 @@
 import './Explore.css'
-import { Drawer, Footer, Navbar, VideoCard } from '../../Component'
+import { Footer, Navbar, VideoCard } from '../../Component'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 
 const Explore = () => {
+const [res, setResponse] = useState([])
+const fetchVideos = async () => {
+  try{
+    const response = await axios.get('/api/videos')
+    console.log(response.data.videos)
+    setResponse(response.data.videos)
+  }
+  catch(err){
+    console.log(err)
+  }
+}
+
+ useEffect(fetchVideos,[])
+
   return (
     <>
       <main className="exploreMain">
         <Navbar />
-        <div>
-          <Drawer />
-        </div>
           <div className="exploreDiv page">
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
-            <VideoCard />
+          {res.map(({_id, title, videoLength, thumbnail, channelName, channelProfile}) => {
+            return(
+
+            <VideoCard key={_id}
+                      title={title}
+                      videoLength={videoLength}
+                      thumbnail={thumbnail}
+                      channelName={channelName}
+                      channelProfile={channelProfile}
+                       />
+            )
+          })}
           </div>
       </main>
+      {/* <div> */}
       <Footer />
+        {/* </div> */}
     </>
   )
 }
