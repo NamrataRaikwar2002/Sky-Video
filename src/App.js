ca362513e2bd594fccabc9dc9128879fa740d2be
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-04-12 14:38:15 +0530

    feat: added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css'
 import { Explore } from './Pages/VideoListing/Explore'
-
+import { Routes, Route } from 'react-router-dom'
+import { Login } from './Pages'
+import { Signup } from './Pages/Authentication/Signup'
 function App() {
   return (
     <>
-      <Explore />
+      <Routes>
+        <Route path="/" element={<Explore />} />
+        <Route path="/login-page" element={<Login />} />
+        <Route path="/signup-page" element={<Signup />} />
+      </Routes>
     </>
   )
 }
