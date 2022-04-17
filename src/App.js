9480fdbdff6a65bc961aefa792f10c7084919573
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-04-17 15:54:54 +0530

    fix: playlist modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css'
 import MockmanEs from 'mockman-js'
-import { Explore } from './Pages/VideoListing/Explore'
 import { Routes, Route } from 'react-router-dom'
-import { Login } from './Pages'
-import { Signup } from './Pages/Authentication/Signup'
+import { Explore, Login, MyPlaylist, Signup } from './Pages'
+import { SinglePlaylist } from './Pages/SinglePlaylist/SinglePlaylist'
+import { Playlist } from './Component'
+import { usePlaylistModal } from './hooks/context/PlaylistModalContext'
+
 function App() {
+  const { modalState } = usePlaylistModal()
+  const { modalStatus } = modalState
   return (
     <>
+      {modalStatus ? <Playlist /> : null}
       <Routes>
         <Route path="/mock" element={<MockmanEs />} />
         <Route path="/" element={<Explore />} />
         <Route path="/login-page" element={<Login />} />
         <Route path="/signup-page" element={<Signup />} />
+        <Route path="/MyPlaylist-page" element={<MyPlaylist />} />
+        <Route
+          path="/MyPlaylist-page/:playlistId"
+          element={<SinglePlaylist />}
+        />
       </Routes>
     </>
   )
