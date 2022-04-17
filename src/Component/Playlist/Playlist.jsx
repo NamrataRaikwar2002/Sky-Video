919cae752cffad6b96b68ccd7bcd55a8ace9ca12
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-04-17 16:00:16 +0530

    feat: playlist modal

---
+++ src/Component/Playlist/Playlist.jsx
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+import './Playlist.css'
+import { useAuth } from '../../hooks/context/AuthContext'
+import { usePlaylist } from '../../hooks/context/PlaylistContext'
+import { createPlaylist } from '../../services'
+import { usePlaylistModal } from '../../hooks/context/PlaylistModalContext'
+import { addVideoToPlaylist } from '../../services/playlistsServices/addVideoToPlaylist'
+import { deleteVideoFromPlaylist } from '../../services/playlistsServices/deleteVideoFromPlaylist'
+
+export const Playlist = ({ _id }) => {
+  const [playlistName, setplaylistName] = useState({ title: '' })
+  const [checkChecked, setcheckChecked] = useState(false)
+  const { modalState, modalDispatch } = usePlaylistModal()
+  const { video } = modalState
+  const { userDetail } = useAuth()
+  const { token } = userDetail
+  const { playlistState, playlistDispatch } = usePlaylist()
+  const { playlists } = playlistState
+
+  const playlistInputHandler = (e) => {
+    setplaylistName({ ...playlistName, title: e.target.value })
+  }
+
+  const checkPlaylist = (title) => {
+    if (playlists.find((item) => item.title === title)) {
+      alert('Playlist already exist')
+    } else if (playlistName.title === '') {
+      alert('Please Enter Playlist Name')
+    } else {
+      return true
+    }
+  }
+
+  const createPlaylistHandler = () => {
+    if (checkPlaylist(playlistName.title)) {
+      createPlaylist(playlistName, playlistDispatch, token, video)
+    }
+    setplaylistName({ ...playlistName, title: '' })
+  }
+
+  const checkVideoInPlaylist = (videos) => {
+      const check = videos.some((item) => item._id === video._id)
+      return check
+  }
+
+  const videoInPlaylistHandler = (e, _id, video) => {
+    const checkVideo = e.target.checked
+    modalDispatch({ type: 'MODAL_CLOSE' })
+    if (checkVideo) {
+      addVideoToPlaylist(_id, video, token, playlistDispatch)
+    } else {
+      deleteVideoFromPlaylist(_id, video._id, token, playlistDispatch)
+    }
+  }
+  return (
+    <>
+      <div
+        className="overlay"
+        onClick={() => modalDispatch({ type: 'MODAL_CLOSE' })}
+      ></div>
+      <div className="playlistDiv">
+        <h3 className="borderBottom playlistHeader">
+          Create New Playlist
+          <p>
+            <i
+              className="fa-solid fa-xmark crossIcon cursorPointer"
+              onClick={() => modalDispatch({ type: 'MODAL_CLOSE' })}
+            ></i>
+          </p>
+        </h3>
+        {playlists ?
+          playlists.map(({ _id, title, videos }) => {
+          return (
+            <div key={_id} className="presenPlaylist">
+              <input
+                type="checkbox"
+                id={_id}
+                checked={checkVideoInPlaylist(videos)}
+                className="borderBottom"
+                onChange={(e) =>videoInPlaylistHandler(e, _id, video)}
+              />
+              <label htmlFor={_id}>{title}</label>
+            </div>
+          )
+        }) : null}
+        <input
+          type="text"
+          className="playlistInput borderBottom"
+          value={playlistName.title}
+          placeholder="Create new playlist"
+          onChange={playlistInputHandler}
+        />
+        <p
+          className="createButton cursorPointer"
+          onClick={createPlaylistHandler}
+        >
+          Create
+        </p>
+      </div>
+    </>
+  )
+}
