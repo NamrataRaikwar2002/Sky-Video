f79a53fd030bc249a0b08358fbd72d82aed6811d
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-04-28 01:09:24 +0530

    feat: create playlist on playlist page

--- src/Component/Playlist/Playlist.jsx
+++ src/Component/Playlist/Playlist.jsx
@@ -6,16 +6,17 @@ import { createPlaylist } from '../../services'
 import { usePlaylistModal } from '../../hooks/context/PlaylistModalContext'
 import { addVideoToPlaylist } from '../../services/playlistsServices/addVideoToPlaylist'
 import { deleteVideoFromPlaylist } from '../../services/playlistsServices/deleteVideoFromPlaylist'
+import { useLocation } from 'react-router'
 
 export const Playlist = ({ _id }) => {
   const [playlistName, setplaylistName] = useState({ title: '' })
-  const [checkChecked, setcheckChecked] = useState(false)
   const { modalState, modalDispatch } = usePlaylistModal()
   const { video } = modalState
   const { userDetail } = useAuth()
   const { token } = userDetail
   const { playlistState, playlistDispatch } = usePlaylist()
   const { playlists } = playlistState
+  const location = useLocation()
 
   const playlistInputHandler = (e) => {
     setplaylistName({ ...playlistName, title: e.target.value })
@@ -32,6 +33,11 @@ export const Playlist = ({ _id }) => {
   }
 
   const createPlaylistHandler = () => {
+    {
+      location.pathname === '/MyPlaylist-page'
+        ? modalDispatch({ type: 'MODAL_CLOSE' })
+        : null
+    }
     if (checkPlaylist(playlistName.title)) {
       createPlaylist(playlistName, playlistDispatch, token, video)
     }
@@ -39,8 +45,8 @@ export const Playlist = ({ _id }) => {
   }
 
   const checkVideoInPlaylist = (videos) => {
-      const check = videos.some((item) => item._id === video._id)
-      return check
+    const check = videos.some((item) => item._id === video._id)
+    return check
   }
 
   const videoInPlaylistHandler = (e, _id, video) => {
@@ -68,21 +74,30 @@ export const Playlist = ({ _id }) => {
             ></i>
           </p>
         </h3>
-        {playlists ?
-          playlists.map(({ _id, title, videos }) => {
-          return (
-            <div key={_id} className="presenPlaylist">
-              <input
-                type="checkbox"
-                id={_id}
-                checked={checkVideoInPlaylist(videos)}
-                className="borderBottom"
-                onChange={(e) =>videoInPlaylistHandler(e, _id, video)}
-              />
-              <label htmlFor={_id}>{title}</label>
-            </div>
-          )
-        }) : null}
+        {playlists
+          ? playlists.map(({ _id, title, videos }) => {
+              return (
+                <div key={_id} className="presenPlaylist">
+                  {location.pathname === '/MyPlaylist-page' ? (
+                    <ul className="playlistList">
+                      <li>{title}</li>
+                    </ul>
+                  ) : (
+                    <>
+                      <input
+                        type="checkbox"
+                        id={_id}
+                        checked={checkVideoInPlaylist(videos)}
+                        className="borderBottom"
+                        onChange={(e) => videoInPlaylistHandler(e, _id, video)}
+                      />
+                      <label htmlFor={_id}>{title}</label>
+                    </>
+                  )}
+                </div>
+              )
+            })
+          : null}
         <input
           type="text"
           className="playlistInput borderBottom"
