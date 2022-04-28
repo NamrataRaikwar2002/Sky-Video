c1fb24f4cfd80d2f946034e142767021807051a8
NamrataRaikwar2002 <92174796+NamrataRaikwar2002@users.noreply.github.com>
2022-04-28 15:50:01 +0530

    feat: added loader

--- src/Pages/VideoListing/Explore.jsx
+++ src/Pages/VideoListing/Explore.jsx
@@ -2,15 +2,19 @@ import './Explore.css'
 import { Footer, Navbar, VideoCard } from '../../Component'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import './loader.css'
 
 const Explore = () => {
   const [res, setResponse] = useState([])
   const [searchInput, setSearchInput] = useState('')
   const [searchResult, setSearchResult] = useState([])
+  const [loader, setLoader] = useState(false)
   const fetchVideos = async () => {
     try {
+      setLoader(true)
       const response = await axios.get('/api/videos')
       setResponse(response.data.videos)
+      setLoader(false)
     } catch (error) {
       console.error(error)
     }
@@ -32,34 +36,42 @@ const Explore = () => {
 
   return (
     <>
-      <main className="exploreMain">
-        <Navbar searchInput={searchInput} searchHandler={searchHandler} />
-        <div className="exploreDiv page">
-          {(searchInput.length < 1 ? res : searchResult).map(
-            ({
-              _id,
-              title,
-              videoLength,
-              thumbnail,
-              channelName,
-              channelProfile,
-            }) => (
-              <VideoCard
-                key={_id}
-                _id={_id}
-                title={title}
-                videoLength={videoLength}
-                thumbnail={thumbnail}
-                channelName={channelName}
-                channelProfile={channelProfile}
-                videos={res}
-              />
-            ),
-          )}
-        </div>
+      <main>
+        <main className="exploreMain">
+          <Navbar searchInput={searchInput} searchHandler={searchHandler} />
+          <div className="exploreDiv page">
+            {loader ? (
+              <div className="loader"></div>
+            ) : (
+              (searchInput.length < 1
+                ? res
+                : searchResult
+              ).map(
+                ({
+                  _id,
+                  title,
+                  videoLength,
+                  thumbnail,
+                  channelName,
+                  channelProfile,
+                }) => (
+                  <VideoCard
+                    key={_id}
+                    _id={_id}
+                    title={title}
+                    videoLength={videoLength}
+                    thumbnail={thumbnail}
+                    channelName={channelName}
+                    channelProfile={channelProfile}
+                    videos={res}
+                  />
+                ),
+              )
+            )}
+          </div>
+        </main>
+        <Footer />
       </main>
-
-      <Footer />
     </>
   )
 }
